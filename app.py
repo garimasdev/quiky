8426800eb34accfb90da56d470684bca49b91e3a
garima-l5 <garima.sachdeva@layerfive.com>
2025-05-15 18:17:16 +0530

    changes in webapp

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
